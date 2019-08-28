77ae353ab1a9c44378821003133e8e3b5e0fcf92
oalvher <oalvher95@gmail.com>
2019-08-28 17:56:20 +0100

    Segundo commit

---
+++ react-tutorial/build/precache-manifest.6abb01cfc250a7bcf5bb41196d7b9848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87dd15897fc89851b3518d0dc02ea354",
+    "url": "/first-page-react/index.html"
+  },
+  {
+    "revision": "584c95e70cb609921c77",
+    "url": "/first-page-react/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/first-page-react/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "584c95e70cb609921c77",
+    "url": "/first-page-react/static/js/main.45ffe2d2.chunk.js"
+  },
+  {
+    "revision": "81b7be56db17d7c535e6",
+    "url": "/first-page-react/static/js/runtime~main.c365cec2.js"
+  }
+]);
--- react-tutorial/build/service-worker.js
+++ react-tutorial/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9ad684596d82a6d86f63625de8cc281.js"
+  "/first-page-react/precache-manifest.6abb01cfc250a7bcf5bb41196d7b9848.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/first-page-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
