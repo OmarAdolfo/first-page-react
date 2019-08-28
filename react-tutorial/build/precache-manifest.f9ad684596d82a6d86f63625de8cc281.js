bed54a7b129536dda0ff664b4b3d69f6c2f61495
oalvher <oalvher95@gmail.com>
2019-08-28 17:53:02 +0100

    Primer commit

---
+++ react-tutorial/build/precache-manifest.f9ad684596d82a6d86f63625de8cc281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9031766db1c60d7dc3d1370998db66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584c95e70cb609921c77",
+    "url": "/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "584c95e70cb609921c77",
+    "url": "/static/js/main.45ffe2d2.chunk.js"
+  },
+  {
+    "revision": "765801b7eb73936e9c4e",
+    "url": "/static/js/runtime~main.11910b7e.js"
+  }
+]);
